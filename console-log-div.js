a423b59f873b4589e8f70e92deffb60c58df3fe2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-01-18 17:13:48 -0500

    Updates

---
+++ console-log-div.js
@@ -0,0 +1,24 @@
+(function initConsoleLogDiv() {
+
+  var log = console.log.bind(console);
+  var logTo = document.createElement('div');
+  logTo.style.width = '100%';
+  logTo.style.minHeight = '200px';
+  logTo.style.fontFamily = 'monospace';
+  logTo.style.marginTop = '20px';
+  document.body.appendChild(logTo);
+
+  function toString(x) {
+    return typeof x === 'string' ? x : JSON.stringify(x);
+  }
+
+  console.log = function () {
+    log.apply(null, arguments);
+    var msg = Array.prototype.slice.call(arguments, 0)
+      .map(toString)
+      .join(' ');
+    var text = logTo.innerText;
+    logTo.innerText = text + msg + '\n';
+  };
+
+}());
