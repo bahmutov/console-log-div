1d392246748f2e1b537f56516ad959e862e70311
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-01-23 11:20:05 -0500

    refactoring, preventing multiple initialization

--- console-log-div.js
+++ console-log-div.js
@@ -1,25 +1,36 @@
 (function initConsoleLogDiv() {
 
-  var log = console.log.bind(console);
-  var logTo = document.createElement('div');
-  logTo.classList.add('console-log-div');
-  logTo.style.width = '100%';
-  logTo.style.minHeight = '200px';
-  logTo.style.fontFamily = 'monospace';
-  logTo.style.marginTop = '20px';
-  document.body.appendChild(logTo);
+  /* eslint no-use-before-define:0 */
+  if (console.log === logWithCopy) {
+    return;
+  }
 
   function toString(x) {
     return typeof x === 'string' ? x : JSON.stringify(x);
   }
 
-  console.log = function () {
+  var log = console.log.bind(console);
+
+  var logTo = (function createLogDiv() {
+    var div = document.createElement('div');
+    div.classList.add('console-log-div');
+    div.style.width = '100%';
+    div.style.minHeight = '200px';
+    div.style.fontFamily = 'monospace';
+    div.style.marginTop = '20px';
+    document.body.appendChild(div);
+    return div;
+  }());
+
+  function logWithCopy() {
     log.apply(null, arguments);
     var msg = Array.prototype.slice.call(arguments, 0)
       .map(toString)
       .join(' ');
     var text = logTo.innerText;
     logTo.innerText = text + msg + '\n';
-  };
+  }
+
+  console.log = logWithCopy;
 
 }());
